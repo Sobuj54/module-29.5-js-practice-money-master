819de5d66bfd683200bdac0218fe5e4357aaf7dc
sobuj <sobujahmed124@gmail.com>
2023-02-18 00:33:47 +0600

    formatted

--- js/savingRemainBalance.js
+++ js/savingRemainBalance.js
@@ -1,16 +1,15 @@
+document.getElementById("btn-save").addEventListener("click", function () {
+  const saveField = getElementValueById("save-field");
+  const savingPercent = saveField / 100;
 
-document.getElementById('btn-save').addEventListener('click', function(){
-    const saveField = getElementValueById('save-field');
-    const savingPercent = saveField/100;
+  const totalIncome = getElementValueById("income-field");
+  const savingString = (totalIncome * savingPercent).toFixed(2);
+  const saving = parseFloat(savingString);
 
-    const totalIncome = getElementValueById('income-field');
-    const savingString = (totalIncome * savingPercent).toFixed(2) ;
-    const saving = parseFloat(savingString);
+  setElementById("saving-amount", saving);
 
-    setElementById('saving-amount',saving);
-
-    const balanceTotal = getElementById('balance-total');
-    const remainingBalance = balanceTotal - saving ;
-    // setting value
-    setElementById('remaining-balance',remainingBalance);
-})
+  const balanceTotal = getElementById("balance-total");
+  const remainingBalance = balanceTotal - saving;
+  // setting value
+  setElementById("remaining-balance", remainingBalance);
+});
